a04eaa8d091a9a8ce6474c1e91789f5d80c0d856
Xiaodong <yxd59493942@gmail.com>
2017-02-06 11:16:28 +0100

    update strategy

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ from MyStrategy import StraightStrategy, RandomStrategy
 
 team1 = SoccerTeam(name="team1",login="etu1")
 team2 = SoccerTeam(name="team2",login="etu2")
-team1.add("John",StraightStrategy()) 
-team2.add("Paul",RandomStrategy())   #Strategie aleatoire
-team2.add("Thomas",RandomStrategy())   #Strategie aleatoire
+team1.add("John",StraightStrategy2()) 
+team2.add("Paul",StraightStrategy1())   
+team2.add("Thomas",StraightStrategy2()) 
