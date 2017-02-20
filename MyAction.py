423d0a8232c64c71cfa25d8c35c3213d25aed946
Xiaodong <yxd59493942@gmail.com>
2017-02-20 18:52:40 +0100

    Update team strategy.

--- MyAction.py
+++ MyAction.py
@@ -13,25 +13,35 @@ class MyAction:
 		return SoccerAction(acc, None)
 
 	def Pass(self, pos1, pos2, normalize = False, scale = 1):
-		dire = MyState.get_vecDist(pos1, pos2).scale(scale)
+		dire = MyState.get_vecDist(pos1, pos2)
 		if normalize:
 			dire = dire.normalize()
+		dire = dire.scale(scale)
 		return SoccerAction(Vector2D(), dire)
 
 	def shoot(self, pos_player, pos_goal, scale = 1):
-		dire = MyState.get_vecDist(pos_player, pos_goal).scale(scale)
+		dire = MyState.get_vecDist(pos_player, pos_goal).normalize()
+		dist_goal = pos_player.distance(pos_goal)
+		if scale == 1:
+			if dist_goal <= 20:
+				scale = 2
+			else :
+				scale = 2 + (dist_goal - 20) / 10.0
+		dire = dire.scale(scale)
 		# f = open('shootdata.txt', 'a')
 		# f.write("Player position" + str(dire) +'\n')
 		return SoccerAction(Vector2D(), dire)
 		
-	def kick(self, pos_player, pos_ball, normalize = True, scale = 1):
-		return self.Pass(pos_player, pos_ball, normalize, scale)
+	def kick(self, pos_player, pos_dest, normalize = True, scale = 1):
+		return self.Pass(pos_player, pos_dest, normalize, scale)
 
 	def canKick(self, pos_player, pos_ball):
 		return pos_player.distance(pos_ball) <= (settings.PLAYER_RADIUS + settings.BALL_RADIUS)
 
 	def canShoot(self, pos_player, pos_ball, pos_goal):
-		return self.canKick(pos_player, pos_ball) and pos_player.distance(pos_goal) <= 30
+		return self.canKick(pos_player, pos_ball) and pos_player.distance(pos_goal) <= 40
 
 	def reachPosition(self, pos_player, pos):
 		return pos_player.distance(pos) <= 2
+	def near(self, pos1, pos2, threshold = 35):
+		return pos1.distance(pos2) <= threshold
--- MyStrategy.py
+++ MyStrategy.py
@@ -14,6 +14,14 @@ class RandomStrategy(Strategy):
     def compute_strategy(self,state,id_team,id_player):
         return SoccerAction(Vector2D.create_random(),Vector2D.create_random())
 
+class ShootStrategy(Strategy):
+    def __init__(self, angle, norm):
+        Strategy.__init__(self,"Shoot")
+        self.angle = angle
+        self.norm = norm
+    def compute_strategy(self,state,id_team,id_player):
+        return SoccerAction(angle = self.angle, norm = self.norm)
+
 class StraightStrategy1(Strategy):
     def __init__(self):
         Strategy.__init__(self,"Straight")
@@ -31,7 +39,7 @@ class StraightStrategy1(Strategy):
             # print "player's velocity is "
             # print mystate.get_vitPlayer(id_team, id_player)
             # print "going to the ball!"
-            return myaction.go(pos_player, pos_ball)
+            return myaction.go(pos_player, pos_ball, 2)
         else: 
             # print "kicking to the ball!"
             return myaction.kick(pos_player, pos_goal)
@@ -83,6 +91,42 @@ class StraightStrategy3(Strategy):
             return myaction.go(pos_player, pos_shoot, 0.5)
         else :
             return myaction.go(pos_player, pos_ball)
+
+class DefendStrategy(Strategy):
+    def __init__(self, type):
+        Strategy.__init__(self,"Straight")
+        self.type = type
+    def compute_strategy(self,state,id_team,id_player):
+        mystate = MyState(state)
+        myaction = MyAction()
+        pos_ball = mystate.get_posBall()
+        pos_player = mystate.get_posPlayer(id_team, id_player)
+        pos_def = mystate.get_posGoal(id_team % 2  + 1)
+        if self.type == 1 :
+            pos_def.y += 2
+        elif self.type == 2:
+            pos_def.y -= 2
+        else:
+            pass
+        pos_goal = mystate.get_posGoal(id_team)
+        # print str(id_player) + ' ' + str(pos_player) + ' ' + str(pos_shoot)
+        if myaction.near(pos_player, pos_goal, 45) and myaction.canKick(pos_player, pos_ball):
+            print "shooting the ball"
+            return myaction.shoot(pos_player, pos_goal, 5)
+
+        if myaction.canKick(pos_player, pos_ball):
+            # print "shooting the ball"
+            return myaction.shoot(pos_player, pos_goal, 2)
+        if myaction.near(pos_player, pos_ball) :
+            return myaction.go(pos_player, pos_ball)
+
+        if not myaction.reachPosition(pos_player, pos_def):
+            # print "player's velocity is "
+            # print mystate.get_vitPlayer(id_team, id_player)
+            # print "going to the point!"
+            # print str(pos_player) + " to " + str(pos_shoot) + '\n'
+            return myaction.go(pos_player, pos_def)
+
 
 
 if __name__ == '__main__':
