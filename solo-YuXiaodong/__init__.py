4f0d2ce6330770ae18c5cad856cf9aa593f61876
yxd117 <yxd59493942@gmail.com>
2017-04-24 10:38:19 +0200

    tme-solo

--- solo-YuXiaodong/__init__.py
+++ solo-YuXiaodong/__init__.py
@@ -1,1 +1,17 @@
-Empty
+from MyGolf import DirectStrategy_slalom, DirectStrategy_golf
+from soccersimulator.mdpsoccer import SoccerTeam
+def get_golf_team():
+	team = SoccerTeam()
+	team.add("John",DirectStrategy_golf())
+	return team
+	
+	
+def get_slalom_team1():
+	team = SoccerTeam()
+	team.add("John",DirectStrategy_slalom())
+	return team
+def get_slalom_team2():
+	team = SoccerTeam()
+	team.add("John",DirectStrategy_slalom())
+	team.add("Paul",DirectStrategy_slalom())
+	return team
